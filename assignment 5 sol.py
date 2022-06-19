0f495cb0ab8c2ddff2a4515074a3502d93815ae7
yuvraj69singh <104338453+yuvraj69singh@users.noreply.github.com>
2022-06-19 19:36:28 +0530

    Add files via upload

---
+++ assignment 5 sol.py
@@ -0,0 +1,64 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=============== RESTART: C:\Users\lenovo\Desktop\assignment 5.py ===============
+Please enter the string: Hello World
+dlroW olleH
+Enter the minimum value :12
+Enter the maximum value: 20
+Enter the number for divisibility check :2
+12
+14
+16
+18
+20
+Enter the length of side:2
+Enter the length of side:5
+Enter the length of side:6
+4.683748498798798
+Enter the no. of rows :5
+*
+**
+***
+**
+*
+Enter the number of rows6
+
+A
+BC
+DEF
+GHIJ
+KLMNO
+PQRSTU
+Enter the Lowest Range Value : 12
+Enter the Upper Range Value : 30
+The Prime Numbers in this range are : 
+13
+17
+19
+23
+29
+The numbers divisible by 11 and are multiple of 7 are : [77, 154, 231, 308, 385, 462]
+Enter the number 1
+Enter the number 2
+Enter the number 3
+Enter the number 4
+Enter the number 5
+Enter the number 6
+Enter the number 7
+Enter the number 8
+Enter the number 9
+Enter the number 10
+Positive numbers are:  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+Negative numbers are:  []
+Even numbers are:  [2, 4, 6, 8, 10]
+Odd numbers are:  [1, 3, 5, 7, 9]
+Number of times each number occurs in the List {1: 1, 2: 1, 3: 1, 4: 1, 5: 1, 6: 1, 7: 1, 8: 1, 9: 1, 10: 1}
+Number of words: 6
+Enter the word: Hello
+Enter the word: World
+Enter the word: Python
+Enter the word: Hello
+Enter the word: Intro to Computing
+Enter the word: Tushi
+Number of occurences:  {'Hello': 2, 'World': 1, 'Python': 1, 'Intro to Computing': 1, 'Tushi': 1}
